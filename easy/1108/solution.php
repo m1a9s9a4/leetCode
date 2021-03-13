49febfc60636dc1615713250831a5214167e6929
masafumi kato <xslash1105@gmail.com>
2021-03-13 22:51:59 +0900

    add: php solution for 1108

---
+++ easy/1108/solution.php
@@ -0,0 +1,11 @@
+<?php
+class Solution {
+
+    /**
+     * @param String $address
+     * @return String
+     */
+    function defangIPaddr($address) {
+        return str_replace('.', '[.]', $address);
+    }
+}
