5e75dad8fd9b3fca023815a7f36d816ba95cd00c
masafumi kato <xslash1105@gmail.com>
2021-03-15 02:27:30 +0900

    add: js solution for 1470

---
+++ easy/1470/solution.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+var shuffle = function(nums, n) {
+    const firstHalf = nums.slice(0, n);
+    const secondHalf = nums.slice(n, nums.length)
+    let response = [];
+    for (let i = 0; i < n; i++) {
+        response.push(firstHalf[i]);
+        response.push(secondHalf[i]);
+    }
+    return response;
+};
