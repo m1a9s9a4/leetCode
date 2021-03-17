75f810984999e2f2f710e0d34926a97c43470c20
masafumi kato <xslash1105@gmail.com>
2021-03-18 00:16:30 +0900

    add: js solution for 1603

---
+++ easy/1603/solution.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} big
+ * @param {number} medium
+ * @param {number} small
+ */
+var ParkingSystem = function(big, medium, small) {
+    this.counts = [big, medium, small]
+};
+
+/** 
+ * @param {number} carType
+ * @return {boolean}
+ */
+ParkingSystem.prototype.addCar = function(carType) {
+    this.counts[carType-1]--
+    return this.counts[carType - 1] >= 0
+};
+
+/** 
+ * Your ParkingSystem object will be instantiated and called as such:
+ * var obj = new ParkingSystem(big, medium, small)
+ * var param_1 = obj.addCar(carType)
+ */
