6a7d5ca0e7b9200ab2e58e65635748e4dd3bbb3d
masafumi kato <xslash1105@gmail.com>
2021-03-21 23:49:15 +0900

    add: js solution for 1528

---
+++ easy/1528/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function(s, indices) {
+    let result = [];
+    let t = '';
+    for (let i = 0; i < indices.length; i++) {
+        result[indices[i]] = s[i]
+    }
+    return result.join('')
+};
